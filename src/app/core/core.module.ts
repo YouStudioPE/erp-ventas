f2f363a461b75b7f7ddd6700f981643cb8e27751
Jesus Romero <romejes1@outlook.com>
2022-09-17 17:43:32 -0500

    feat: build basic structure for 'core' and 'shared' modules to start project development

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '@shared/index';
+
+@NgModule({
+  declarations: [],
+  imports: [SharedModule],
+})
+export class CoreModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { AngularModule } from './angular';
+
+@NgModule({
+  declarations: [],
+  imports: [AngularModule],
+})
+export class SharedModule {}
