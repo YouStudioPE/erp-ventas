4863b1a4bb3fce58078c12b2dbd338b481775c07
Jesus Romero <romejes1@outlook.com>
2022-09-30 00:14:02 -0500

    feat: Build basic structure for 'layout' module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from './core';
 import { AppComponent, AppRoutingModule } from './index';
+import { LayoutModule } from './layout';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
